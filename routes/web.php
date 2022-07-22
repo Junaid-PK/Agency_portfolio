e8620019c2d9cc3658c0fe1deac3dd0ff949adcf
JUNAID-PK <junaidhussnain369@gmail.com>
2022-07-22 10:52:14 +0500

    Step 1-> Integrated admin template

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,7 +19,9 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('admin.index');
 })->middleware(['auth'])->name('dashboard');
+
+Route::get('/admin/logout', [AdminController::class, 'destroy'])->name('admin.logout');
 
 require __DIR__.'/auth.php';
