5973c16dbe1a056db400d82d5c73d1c760710efb
JUNAID-PK <junaidhussnain369@gmail.com>
2022-07-26 20:50:49 +0500

    Footer Page Setup Completed

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Home\AboutController;
 use App\Http\Controllers\Home\PortfolioController;
 use App\Http\Controllers\Home\BlogCategoryController;
 use App\Http\Controllers\Home\BlogController;
+use App\Http\Controllers\Home\FooterController;
 
 
 Route::get('/', function () {
@@ -88,6 +89,14 @@ Route::controller(HomeSliderController::class)->middleware(['auth'])->group(func
     Route::get('/blog/details/{id}', 'BlogDetails')->name('blog.details');
     Route::get('/category/blog/{id}', 'CategoryBlog')->name('category.blog');
     Route::get('/blog', 'HomeBlog')->name('home.blog');
+});
+
+
+ // Footer All Route 
+ Route::controller(FooterController::class)->group(function () {
+    Route::get('/footer/setup', 'FooterSetup')->name('footer.setup');
+    Route::post('/update/footer', 'UpdateFooter')->name('update.footer');
+
 });
 
 Route::get('/dashboard', function () {
